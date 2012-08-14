e8fddf242905a3af13be1ac39b80773c0394e71d
Gene Ressler <gene.ressler@gmail.com>
2012-08-14 18:23:30 -0400

    Initial screen and test

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "Registrations" do
-  describe "GET /registrations" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get registrations_path
-      response.status.should be(200)
+
+  describe "Login/Register Page" do
+    it "should have content" do
+      
+      visit new_session_path
+      page.should have_content('Register and Log In')
     end
   end
+
 end
