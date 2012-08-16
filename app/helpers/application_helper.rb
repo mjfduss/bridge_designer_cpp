c2ac09cfbaca6547814c7be33f316b6ae25a1132
Gene Ressler <gene.ressler@gmail.com>
2012-08-16 18:37:08 -0400

    Pretty forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+include ActionView::Helpers::FormOptionsHelper
+
 module ApplicationHelper
+
+  @@pairs = 
+    [
+     ['--Select Here--', '0']
+    ]
+
+  @@options =  options_for_select(@@pairs, '0')
+
+  def self.state_select(f, id)
+    return f.select id, @@options
+  end
+
 end
