e09b855ea569947104d13c7dbf836ea7fe179ca4
Gene Ressler <gene.ressler@gmail.com>
2012-11-28 22:25:42 -0500

    Table existence check in sequence number initializer

--- config/initializers/sequence_numbers.rb
+++ config/initializers/sequence_numbers.rb
@@ -1,4 +1,3 @@
 # Initialize sequence numbers if not already there.  Value defaults to zero.
-%w{design}.each do |tag|
-  SequenceNumber.find_or_create_by_tag(tag, :value => 0)
-end
+SequenceNumber.find_or_create_by_tag('design', :value => 0) \
+  if ActiveRecord::Base.connection.table_exists? 'sequence_numbers'
