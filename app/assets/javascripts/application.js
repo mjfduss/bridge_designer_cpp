4369161b06dbd3352c7bcf221986f29c9697fffc
Gene Ressler <gene.ressler@gmail.com>
2012-11-28 23:53:16 -0500

    Fixed html5shiv reference for precompiler

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,1 @@
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require_self
-//= require custom.js.coffee
-//= require html5shiv.js
+//= require custom
