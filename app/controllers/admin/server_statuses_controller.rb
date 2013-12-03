06e94da1226278a3f36d0cf98546d11327ef5bca
Gene Ressler <gene.ressler@gmail.com>
2013-12-02 23:26:38 -0500

    Database reset function

--- app/controllers/admin/server_statuses_controller.rb
+++ app/controllers/admin/server_statuses_controller.rb
@@ -1,9 +1,64 @@
+require 'rake'
+
 class Admin::ServerStatusesController < Admin::ApplicationController
+
+  VERIFICATION = 'irreversible'
+
   def edit
-    @time = Time.now.strftime("%H:%M Eastern on %d-%m-%Y")
     @ip = request.remote_ip
   end
 
   def update
+    ok = true;
+    unless params[:verification].downcase.strip == VERIFICATION
+      flash[:alert_verification] = "Verification is not correct. Type '#{VERIFICATION}' in the space provided."
+      ok = false
+    end
+    admin = Administrator.find(session[:admin_id])
+    unless admin && admin.authenticate(params[:password])
+      flash[:alert_password] = 'Password is incorrect.  Please enter your administrator password.'
+      ok = false
+    end
+    if ok
+      flash[:alert] = 'Database would have been cleared if this feature were enabled.'
+      logger.fatal "Database was cleared by admin #{admin.name}."
+    else
+      flash[:alert] = 'Database was not cleared.'
+      logger.fatal "Database clear failed for admin #{admin.name}."
+    end
+    render 'edit'
+  end
+
+  def self.reset_all
+    # Clear the unofficial standings database.
+    REDIS::flushall
+
+    # Clear relevant SQL tables.
+    # Skipped:
+    #   Administrator    accounts are still needed
+    #   CkeditorAssets   pictures for rich HTML documents
+    #   CkeditorContents rich HTML documents
+    #   DelayedJobs      might confuse DJ
+    #   Groups           could be handy for reuse
+    #   HtmlDocuments    rich HTML document headers
+    #   LocalContests    obviously still useful!
+    #   LocalScoreboards will be regenerated anywan
+    #   ReminderRequests deleted elsewhere
+    #   Schedule         obviously still useful
+    #   Scoreboards      historical record is important
+    #   SequenceNumber   just reset, don't clear
+
+    Affiliation.delete_all
+    Best.delete_all
+    Design.delete_all
+    Member.delete_all
+    Parent.delete_all
+    PasswordReset.delete_all
+    SequenceNumber.reset :design
+    Team.delete_all
+
+    # Reset the affiliation counter caches in each of the local contests.
+    LocalContest.pluck(:id).each { |id| LocalContest.reset_counters(id, :affiliations) }
   end
 end
+
--- lib/tasks/reset.rake
+++ lib/tasks/reset.rake
@@ -1,11 +1,4 @@
 desc "Clean both the PostgreSQL and REDIS database."
 task :clear_all => :environment do
-  REDIS::flushall
-  Affiliation.delete_all
-  Best.delete_all
-  Design.delete_all
-  Member.delete_all
-  PasswordReset.delete_all
-  SequenceNumber.reset :design
-  Team.delete_all
+  Admin::ServerStatusesController.reset_all
 end
