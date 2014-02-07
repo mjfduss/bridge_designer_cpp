1aafb8643f541b6771af9ef24840c3c4c870cf05
Gene Ressler <gene.ressler@gmail.com>
2014-02-06 23:15:35 -0500

    Roll back ckeditor to 4.0.4

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -1,7 +1,7 @@
 class CertificatesController < ApplicationController
   def show
     @team = Team.find(session[:team_id])
-    if @team.best_design && %w{a 2}.include?(@team.status) && session[:admin_id]
+    if @team.best_design && %w{- a 2}.include?(@team.status) && session[:admin_id]
       @standing, @basis = @team.standing
     else
       # Should never happen because link should not be presented.
